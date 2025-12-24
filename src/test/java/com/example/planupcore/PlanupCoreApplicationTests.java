2e1261eee5caacd9b313810d5a580cb0b1b62988
SeungYeop Ryu <n006h3417h@gmail.com>
2025-12-24 11:49:58 +0900

    chore: begin a project

---
+++ src/test/java/com/example/planupcore/PlanupCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planupcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanupCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
