22e85306d63cfaa8bf78257867f1b00c335c1748
SeungYeop Ryu <n006h3417h@gmail.com>
2026-01-06 14:36:58 +0900

    style: clean imports

--- src/main/java/com/example/planupcore/domain/schedule/dto/ScheduleCreateDto.java
+++ src/main/java/com/example/planupcore/domain/schedule/dto/ScheduleCreateDto.java
@@ -1,6 +1,5 @@
 package com.example.planupcore.domain.schedule.dto;
 
-import com.example.planupcore.domain.schedule.entity.ScheduleType;
 import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
--- src/main/java/com/example/planupcore/domain/schedule/dto/ScheduleUpdateDto.java
+++ src/main/java/com/example/planupcore/domain/schedule/dto/ScheduleUpdateDto.java
@@ -1,7 +1,5 @@
 package com.example.planupcore.domain.schedule.dto;
 
-import com.example.planupcore.domain.schedule.entity.ScheduleType;
-
 import java.time.LocalDateTime;
 
 public record ScheduleUpdateDto(
