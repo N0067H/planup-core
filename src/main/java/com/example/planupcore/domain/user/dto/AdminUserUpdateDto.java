da2284bf72ee63055e6c4bbefe822d149dbfd2e0
SeungYeop Ryu <n006h3417h@gmail.com>
2026-01-10 09:58:43 +0900

    feat(User): add validation to request dto

--- src/main/java/com/example/planupcore/domain/user/dto/AdminUserUpdateDto.java
+++ src/main/java/com/example/planupcore/domain/user/dto/AdminUserUpdateDto.java
@@ -1,11 +1,23 @@
 package com.example.planupcore.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record AdminUserUpdateDto(
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     String email,
 
+    @NotBlank(message = "Nickname is required")
+    @Size(min = 3, max = 50, message = "Nickname must be between 3 and 50 characters")
     String nickname,
 
+    @NotBlank(message = "First name is required")
+    @Size(min = 1, max = 50, message = "First name must be between 1 and 50 characters")
     String firstName,
 
+    @NotBlank(message = "Last name is required")
+    @Size(min = 1, max = 50, message = "Last name must be between 1 and 50 characters")
     String lastName
 ) {}
--- src/main/java/com/example/planupcore/domain/user/dto/UserUpdateDto.java
+++ src/main/java/com/example/planupcore/domain/user/dto/UserUpdateDto.java
@@ -1,19 +1,30 @@
 package com.example.planupcore.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UserUpdateDto (
-
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     String email,
 
+    @NotBlank(message = "Nickname is required")
+    @Size(min = 3, max = 50, message = "Nickname must be between 3 and 50 characters")
     String nickname,
 
+    @NotBlank(message = "First name is required")
+    @Size(min = 1, max = 50, message = "First name must be between 1 and 50 characters")
     String firstName,
 
+    @NotBlank(message = "Last name is required")
+    @Size(min = 1, max = 50, message = "Last name must be between 1 and 50 characters")
     String lastName,
 
     @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     String password,
 
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     String newPassword
 ) {}
